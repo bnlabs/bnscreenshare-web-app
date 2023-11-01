b10b7726efbc434a509996726b35e7f8b5273665
Hy0tic <vothanhdat278@gmail.com>
2023-10-31 22:07:10 -0400

    fix offset of pen and move canvas over video element

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,121 +1,121 @@
-import React, { useRef, useState, useEffect } from 'react';
+// import React, { useRef, useState, useEffect } from 'react';
 
-const Canvas = () => {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const [isPainting, setIsPainting] = useState(false);
-  const [lineWidth, setLineWidth] = useState(5);
-  const [strokeStyle, setStrokeStyle] = useState('#000000');
+// const Canvas = () => {
+//   const canvasRef = useRef<HTMLCanvasElement>(null);
+//   const [isPainting, setIsPainting] = useState(false);
+//   const [lineWidth, setLineWidth] = useState(5);
+//   const [strokeStyle, setStrokeStyle] = useState('#000000');
 
-  useEffect(() => {
-    if (canvasRef.current) {
-        const canvas = canvasRef.current;
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-      }
-  }, []);
+//   useEffect(() => {
+//     if (canvasRef.current) {
+//         const canvas = canvasRef.current;
+//         canvas.width = window.innerWidth;
+//         canvas.height = window.innerHeight;
+//       }
+//   }, []);
 
-  const startPainting = (e: React.MouseEvent) => {
-    const coordinates = getCoordinates(e.nativeEvent);
-    if (coordinates) {
-      setIsPainting(true);
-    }
-  };
+//   const startPainting = (e: React.MouseEvent) => {
+//     const coordinates = getCoordinates(e.nativeEvent);
+//     if (coordinates) {
+//       setIsPainting(true);
+//     }
+//   };
 
-  const endPainting = () => {
-    if (isPainting) {
-        const canvas = canvasRef.current;
-        const ctx = canvas?.getContext('2d');
-        if (ctx) {
-          ctx.stroke();
-          ctx.beginPath();
-        }
-        setIsPainting(false);
+//   const endPainting = () => {
+//     if (isPainting) {
+//         const canvas = canvasRef.current;
+//         const ctx = canvas?.getContext('2d');
+//         if (ctx) {
+//           ctx.stroke();
+//           ctx.beginPath();
+//         }
+//         setIsPainting(false);
     
-        // Set a timeout to clear the canvas after 3 seconds
-        setTimeout(() => {
-          if (canvas && ctx) {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-          }
-        }, 3000); // 3000 milliseconds = 3 seconds
-      }
-  };
+//         // Set a timeout to clear the canvas after 3 seconds
+//         setTimeout(() => {
+//           if (canvas && ctx) {
+//             ctx.clearRect(0, 0, canvas.width, canvas.height);
+//           }
+//         }, 3000); // 3000 milliseconds = 3 seconds
+//       }
+//   };
 
-  const paint = (e: React.MouseEvent) => {
-    if (isPainting && canvasRef.current) {
-        const ctx = canvasRef.current.getContext('2d');
-        const newCoordinates = getCoordinates(e.nativeEvent);
-        if (ctx && newCoordinates) {
-          ctx.lineWidth = lineWidth;
-          ctx.strokeStyle = strokeStyle;
-          ctx.lineCap = 'round';
-          ctx.lineTo(newCoordinates.x, newCoordinates.y);
-          ctx.stroke();
-          ctx.beginPath();
-          ctx.moveTo(newCoordinates.x, newCoordinates.y);
-        }
-      }
-  };
+//   const paint = (e: React.MouseEvent) => {
+//     if (isPainting && canvasRef.current) {
+//         const ctx = canvasRef.current.getContext('2d');
+//         const newCoordinates = getCoordinates(e.nativeEvent);
+//         if (ctx && newCoordinates) {
+//           ctx.lineWidth = lineWidth;
+//           ctx.strokeStyle = strokeStyle;
+//           ctx.lineCap = 'round';
+//           ctx.lineTo(newCoordinates.x, newCoordinates.y);
+//           ctx.stroke();
+//           ctx.beginPath();
+//           ctx.moveTo(newCoordinates.x, newCoordinates.y);
+//         }
+//       }
+//   };
 
-  const getCoordinates = (event: MouseEvent) => {
-    if (!canvasRef.current) {
-      return;
-    }
+//   const getCoordinates = (event: MouseEvent) => {
+//     if (!canvasRef.current) {
+//       return;
+//     }
 
-    const canvas = canvasRef.current;
-    console.log("x offset", canvas.offsetLeft)
-    console.log("y offset", canvas.offsetTop)
-    console.log("x coordinate", event.pageX)
-    console.log("y coordinate", event.pageY)
-    return {
-      x: (event.pageX - canvas.offsetLeft),
-      y: (event.pageY - canvas.offsetTop),
-    };
-  };
+//     const canvas = canvasRef.current;
+//     console.log("x offset", canvas.offsetLeft)
+//     console.log("y offset", canvas.offsetTop)
+//     console.log("x coordinate", event.pageX)
+//     console.log("y coordinate", event.pageY)
+//     return {
+//       x: (event.pageX - canvas.offsetLeft),
+//       y: (event.pageY - canvas.offsetTop),
+//     };
+//   };
 
 
-  const handleToolbar = (event:any) => {
-    const { id, value } = event.target;
-    switch (id) {
-      case 'stroke':
-        setStrokeStyle(value);
-        break;
-      case 'lineWidth':
-        setLineWidth(value);
-        break;
-      default:
-        break;
-    }
-  };
+//   const handleToolbar = (event:any) => {
+//     const { id, value } = event.target;
+//     switch (id) {
+//       case 'stroke':
+//         setStrokeStyle(value);
+//         break;
+//       case 'lineWidth':
+//         setLineWidth(value);
+//         break;
+//       default:
+//         break;
+//     }
+//   };
 
-  const ClearCanvas = () => {
-    const canvas = canvasRef.current;
-    const ctx = canvas?.getContext('2d')
-    if (canvas && ctx) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-      }
-  }
+//   const ClearCanvas = () => {
+//     const canvas = canvasRef.current;
+//     const ctx = canvas?.getContext('2d')
+//     if (canvas && ctx) {
+//         ctx.clearRect(0, 0, canvas.width, canvas.height);
+//       }
+//   }
 
-  return (
-    <div className="bg-slate-500 text-black flex h-full">
+//   return (
+//     <div className="text-black flex h-full">
 
-      <div className="flex flex-col p-5 w-72" id="toolbar" onChange={handleToolbar}>
-        <button className="p-2 border-white border hover:bg-slate-800" id="clear" onClick={()=>ClearCanvas()}>Clear</button>
-        <label className="text-xl">Stroke</label>
-        <input className="w-full" type="color" id="stroke" defaultValue={strokeStyle} />
-        <label className="text-xl">Line Width</label>
-        <input className="" type="number" id="lineWidth" defaultValue={lineWidth} value={lineWidth} />
-      </div>
+//       <div className="flex flex-col p-5 w-72 bg-slate-500" id="toolbar" onChange={handleToolbar}>
+//         <button className="p-2 border-white border hover:bg-slate-800" id="clear" onClick={()=>ClearCanvas()}>Clear</button>
+//         <label className="text-xl">Stroke</label>
+//         <input className="w-full" type="color" id="stroke" defaultValue={strokeStyle} />
+//         <label className="text-xl">Line Width</label>
+//         <input className="" type="number" id="lineWidth" defaultValue={lineWidth} value={lineWidth} />
+//       </div>
 
-      <canvas
-        className='bg-slate-900 h-5/6 w-4/6 rounded-lg'
-        ref={canvasRef}
-        onMouseDown={startPainting}
-        onMouseUp={endPainting}
-        onMouseMove={paint}
-      />
+//       <canvas
+//         className='bg-transparent h-5/6 rounded-lg border-solid border-gray-600 border-8'
+//         ref={canvasRef}
+//         onMouseDown={startPainting}
+//         onMouseUp={endPainting}
+//         onMouseMove={paint}
+//       />
 
-    </div>
-  );
-}
+//     </div>
+//   );
+// }
 
-export default Canvas;
+// export default Canvas;
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -9,6 +9,12 @@ const Video = ({user, defaultMuteValue} : {user:string, defaultMuteValue:boolean
     const [isMuted, setIsMuted] = useState(defaultMuteValue);
     const [volume, setVolume] = useState<number>(1);
 
+    // Canvas states
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const [isPainting, setIsPainting] = useState(false);
+    const [lineWidth, setLineWidth] = useState(5);
+    const [strokeStyle, setStrokeStyle] = useState('#000000');
+
     let id;
     if(user === "1")
     {
@@ -40,6 +46,15 @@ const Video = ({user, defaultMuteValue} : {user:string, defaultMuteValue:boolean
             }
         };
     });
+
+    // canvas useEffect()
+    useEffect(() => {
+        // if (canvasRef.current) {
+        //     const canvas = canvasRef.current;
+        //     canvas.width = window.innerWidth;
+        //     canvas.height = window.innerHeight;
+        //   }
+      }, []);
     
     const toggleMute = () => {
         const video = videoRef.current;
@@ -69,9 +84,113 @@ const Video = ({user, defaultMuteValue} : {user:string, defaultMuteValue:boolean
         setVolume(newVolume);
     };
     
+
+    const startPainting = (e: React.MouseEvent) => {
+        const coordinates = getCoordinates(e.nativeEvent);
+        if (coordinates) {
+          setIsPainting(true);
+        }
+      };
+
+    const endPainting = () => {
+        if (isPainting) {
+            const canvas = canvasRef.current;
+            const ctx = canvas?.getContext('2d');
+            if (ctx) {
+                ctx.stroke();
+                ctx.beginPath();
+            }
+            setIsPainting(false);
+        
+            // Set a timeout to clear the canvas after 3 seconds
+            // setTimeout(() => {
+            //     if (canvas && ctx) {
+            //     ctx.clearRect(0, 0, canvas.width, canvas.height);
+            //     }
+            // }, 3000); // 3000 milliseconds = 3 seconds
+            }
+    };
+
+    const paint = (e: React.MouseEvent) => {
+        if (isPainting && canvasRef.current) {
+            const ctx = canvasRef.current.getContext('2d');
+            const newCoordinates = getCoordinates(e.nativeEvent);
+            if (ctx && newCoordinates) {
+                // Adjust line width according to the scale factors
+                const rect = canvasRef.current.getBoundingClientRect();
+                const scaleX = canvasRef.current.width / rect.width;
+                const scaleY = canvasRef.current.height / rect.height;
+
+                ctx.lineWidth = lineWidth * Math.min(scaleX, scaleY);
+                ctx.strokeStyle = strokeStyle;
+                ctx.lineCap = 'round';
+                ctx.lineTo(newCoordinates.x, newCoordinates.y);
+                ctx.stroke();
+                ctx.beginPath();
+                ctx.moveTo(newCoordinates.x, newCoordinates.y);
+            }
+            }
+    };
+
+    const getCoordinates = (event: MouseEvent) => {
+        if (!canvasRef.current) {
+          return;
+        }
+      
+        const canvas = canvasRef.current;
+        const rect = canvas.getBoundingClientRect();
+      
+        // Adjust coordinates with page scroll for absolute positioning
+        const scaleX = canvas.width / rect.width;    // aspect ratio for width scale
+        const scaleY = canvas.height / rect.height;  // aspect ratio for height scale
+      
+        // Compute the coordinates within the canvas taking scale into consideration
+        const x = (event.clientX - rect.left) * scaleX;
+        const y = (event.clientY - rect.top) * scaleY;
+      
+        console.log("x,y offset", rect.left, rect.top);
+        console.log("x,y", `${event.clientX},${event.clientY}`);
+        console.log("Adjusted x,y", `${x},${y}`);
+      
+        return {
+          x,
+          y,
+        };
+      };
+
+
+    const handleToolbar = (event:any) => {
+        const { id, value } = event.target;
+        switch (id) {
+            case 'stroke':
+            setStrokeStyle(value);
+            break;
+            case 'lineWidth':
+            setLineWidth(value);
+            break;
+            default:
+            break;
+        }
+    };
+
+    const ClearCanvas = () => {
+        const canvas = canvasRef.current;
+        const ctx = canvas?.getContext('2d')
+        if (canvas && ctx) {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            }
+    }
+
     return (<div className='relative'>
-        <StyledVideo ref={videoRef} className="video-player" id={id} autoPlay playsInline onClick={handleFullScreen}>
-        </StyledVideo>
+            <StyledVideo ref={videoRef} className="video-player" id={id} autoPlay playsInline onClick={handleFullScreen}>
+            </StyledVideo>
+            <canvas
+                className='absolute h-73vh w-130vh top-2 left-2 bg-transparent rounded-lg border-solid border-gray-600 border-4'
+                ref={canvasRef}
+                onMouseDown={startPainting}
+                onMouseUp={endPainting}
+                onMouseMove={paint}
+            />
         <div className='buttons my-2'>
             <input
                 className='mx-2'
@@ -86,6 +205,14 @@ const Video = ({user, defaultMuteValue} : {user:string, defaultMuteValue:boolean
                 {isMuted ? <VolumeOffOutlinedIcon/> : <VolumeUpOutlinedIcon/>}
             </Button>
             <Button variant="outline" color="gray" onClick={handleFullScreen}><FullscreenOutlinedIcon/></Button>
+        </div>
+
+        <div className="flex flex-col p-5 w-72 bg-slate-800 rounded-lg" id="toolbar" onChange={handleToolbar}>
+            <button className="p-2 border-white border hover:bg-slate-600" id="clear" onClick={()=>ClearCanvas()}>Clear</button>
+            <label className="">Stroke</label>
+            <input className="w-full bg-slate-700" type="color" id="stroke" defaultValue={strokeStyle} />
+            <label className="">Line Width</label>
+            <input className="text-black bg-slate-600" type="number" id="lineWidth" defaultValue={lineWidth} value={lineWidth} />
         </div>
     </div>)
 }
